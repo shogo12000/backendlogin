137a24209dfec690b177cec0ef37f389cbfe298c
sidney <shogo12000@hotmail.com>
2025-03-03 05:10:32 -0800

    fix vercel

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ app.use('/api/users', userRoutes);
 
 // const PORT = process.env.PORT || 5000 ;
 // app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
-export const handler = serverless(app); 
+export default handler = serverless(app); 
