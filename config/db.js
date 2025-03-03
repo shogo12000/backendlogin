b3cb09aa1334d7b7e06046634de3b459bf534e13
sidney <shogo12000@hotmail.com>
2025-03-03 05:21:23 -0800

    fix vercel

--- config/db.js
+++ config/db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI );
         console.log('Conexão ao MongoDB bem-sucedida!');
     } catch (err) {
         console.error('Erro ao conectar ao MongoDB:', err);
