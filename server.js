dd0d48dc0e1403b8cce5b417fb3e1baadbf11733
sidney <shogo12000@hotmail.com>
2025-03-03 07:01:44 -0800

    fix vercel

--- server.js
+++ server.js
@@ -8,11 +8,21 @@ import serverless from 'serverless-http';
 
 const app = express();
 
+// Definindo as opções do CORS
 const corsOptions = {
-    origin: '*', // Permitir apenas este domínio
+    origin: '*', // Permitir qualquer domínio
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-  };
+};
+
+// Middleware para timeout
+app.use((req, res, next) => {
+    res.setTimeout(30000, () => { // Timeout de 30 segundos
+        console.log('Tempo limite de requisição excedido');
+        res.status(504).send('Gateway Timeout');
+    });
+    next();
+});
 
 const startServer = async () => {
     try {
@@ -36,4 +46,4 @@ const startServer = async () => {
 startServer();
 
 // Exportação para AWS Lambda
-export default serverless(app);
+export const handler = serverless(app);
