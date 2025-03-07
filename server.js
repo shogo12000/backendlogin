209cabf0f8b457898b2d8fd7a38fc7635c6daed5
sidney <shogo12000@hotmail.com>
2025-03-07 04:35:54 -0800

    final version

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import serverless from 'serverless-http'; 
+ 
 
 const app = express();
 connectDB();  
@@ -18,10 +18,10 @@ app.get('/api/test', (req,res)=>{
 })
 
 // Importação das rotas
-// app.use('/api/auth', authRoutes);
-// app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
 
-export default serverless(app)
+ 
